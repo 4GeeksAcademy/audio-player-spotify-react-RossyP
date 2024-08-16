d60352310b68f92b0cc426d1b864c320a2991ce2
Rossy Jasmin Palacin Nuñez <119419018+ROZZYNUEZ14@users.noreply.github.com>
2024-08-16 18:02:32 +0000

    Se inicia la creacion del reproductor de musica

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,24 +1,87 @@
+import { useEffect, useRef, useState } from "react";
 import React from "react";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faPlay } from '@fortawesome/free-solid-svg-icons';
 
-//include images into your bundle
+
 import rigoImage from "../../img/rigo-baby.jpg";
 
-//create your first component
+
 const Home = () => {
+
+	const [allSongs, setAllSongs] = useState([])
+	const [id, setId] = useState("")
+	const [url, setUrl] = useState("https://youtu.be/R8Q8vqVT_HI?si=iLwIrT08au7Ulsr4")
+	const [buttonPlay, setButtonPlay] = useState(false);
+	const reproductorAudio = useRef()
+
+	// console.log(id)
+	// console.log(url)
+	console.log(buttonPlay)
+
+	function listSongs(){
+		fetch(`https://playground.4geeks.com/sound/songs`)
+		.then((response) => response.json())
+		.then((data) => {
+			console.log(data.songs)
+			
+			setAllSongs(currentSongs => currentSongs.concat(data.songs))
+			// let ids = data.songs.map((item) => item.id)
+			// let urls = data.songs.map((item) => item.url)
+
+			// setId(currentId => currentId.concat(ids))
+			// setUrl(currentUrl => currentUrl.concat(urls))
+
+		})
+	}
+
+	function buttonStatus(){
+		setButtonPlay(currentplay => !currentplay)
+		if(!buttonPlay){
+			reproductorAudio.current.play()
+		}else{
+			reproductorAudio.current.pause()
+		}
+	}
+	
+	function obtenerUrl(url){
+		setUrl(url)
+	}
+	useEffect(()=>{
+		listSongs()
+		
+	},[])
+
 	return (
 		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+			<ol>
+			{
+				allSongs.map((item) => {
+					return(
+						<div>
+							<div onClick={()=>obtenerUrl(item.url)}>
+								<li key={item.id}>{item.name}</li>
+							</div>
+						</div>
+						
+					)
+				})
+			}
+			</ol>
+			<div>
+				<div>
+					<audio controls ref={reproductorAudio}>
+						<source src={url} type="audio/mpeg"/>
+						<source src={url} type="audio/ogg"/>
+					</audio>
+				</div>
+				<div>
+					<button onClick={buttonStatus}>
+						<FontAwesomeIcon icon={faPlay} />
+					</button>
+				</div>
+				
+			</div>
 		</div>
 	);
 };
