576b49359f6b0c05d13e6279a03fe972157f205c
Rossy Jasmin Palacin Nuñez <119419018+ROZZYNUEZ14@users.noreply.github.com>
2024-08-16 22:41:41 +0000

    Se crea boton retroceder

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,8 +1,7 @@
 import { useEffect, useRef, useState } from "react";
 import React from "react";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faPlay } from '@fortawesome/free-solid-svg-icons';
-
+import { faForward, faCirclePlay, faPause, faBackward } from '@fortawesome/free-solid-svg-icons';
 
 import rigoImage from "../../img/rigo-baby.jpg";
 
@@ -10,13 +9,16 @@ import rigoImage from "../../img/rigo-baby.jpg";
 const Home = () => {
 
 	const [allSongs, setAllSongs] = useState([])
-	const [id, setId] = useState("")
-	const [url, setUrl] = useState("https://youtu.be/R8Q8vqVT_HI?si=iLwIrT08au7Ulsr4")
+	const [id, setId] = useState()
+	const [index, setIndex] = useState()
+	const [url, setUrl] = useState("")
 	const [buttonPlay, setButtonPlay] = useState(false);
 	const reproductorAudio = useRef()
 
-	// console.log(id)
-	// console.log(url)
+	console.log(id);
+	console.log(url);
+	
+	
 	console.log(buttonPlay)
 
 	function listSongs(){
@@ -26,29 +28,55 @@ const Home = () => {
 			console.log(data.songs)
 			
 			setAllSongs(currentSongs => currentSongs.concat(data.songs))
-			// let ids = data.songs.map((item) => item.id)
-			// let urls = data.songs.map((item) => item.url)
-
-			// setId(currentId => currentId.concat(ids))
-			// setUrl(currentUrl => currentUrl.concat(urls))
-
+	
 		})
 	}
 
+	
 	function buttonStatus(){
+		
 		setButtonPlay(currentplay => !currentplay)
-		if(!buttonPlay){
+	
+		if(buttonPlay){
+			
 			reproductorAudio.current.play()
 		}else{
 			reproductorAudio.current.pause()
 		}
 	}
-	
+
 	function obtenerUrl(url){
 		setUrl(url)
+		reproductorAudio.current.src = "https://playground.4geeks.com" + url
+		reproductorAudio.current.play()
+		setButtonPlay(false)
 	}
+
+	function buttonBack(){
+		allSongs.forEach((item, index) => {
+			if(id === item.id){
+				if(id > 0){
+					reproductorAudio.current.src = "https://playground.4geeks.com" + allSongs[index-1].url
+					console.log(reproductorAudio.current)
+					reproductorAudio.current.play()
+					setId(allSongs[index-1].id)
+				}
+			}
+		})
+	}
+
+
+	function buttonForward(){
+
+	}
+
+	function obtenerId(id){
+		setId(id)
+	}
+
 	useEffect(()=>{
 		listSongs()
+		buttonStatus()
 		
 	},[])
 
@@ -56,28 +84,28 @@ const Home = () => {
 		<div className="text-center">
 			<ol>
 			{
-				allSongs.map((item) => {
+				allSongs.map((item, index) => {
 					return(
-						<div>
-							<div onClick={()=>obtenerUrl(item.url)}>
-								<li key={item.id}>{item.name}</li>
+							<div key={item.id} onClick={()=>{obtenerUrl(item.url), obtenerId(item.id)}}>
+								<li>{item.name}</li>
 							</div>
-						</div>
-						
 					)
 				})
 			}
 			</ol>
 			<div>
 				<div>
-					<audio controls ref={reproductorAudio}>
-						<source src={url} type="audio/mpeg"/>
-						<source src={url} type="audio/ogg"/>
-					</audio>
+					<audio  ref={reproductorAudio}></audio>
 				</div>
 				<div>
+					<button onClick={buttonBack}>
+						<FontAwesomeIcon icon={faBackward} />
+					</button>
 					<button onClick={buttonStatus}>
-						<FontAwesomeIcon icon={faPlay} />
+						{buttonPlay ? <FontAwesomeIcon icon={faCirclePlay} /> : <FontAwesomeIcon icon={faPause} />}
+					</button>
+					<button onClick={buttonForward}>
+						<FontAwesomeIcon icon={faForward} />
 					</button>
 				</div>
 				
